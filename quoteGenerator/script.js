bfea3a1cd3330734d4944ba40236421b09976da6
Sarah Zhong <sarahchungwu@gmail.com>
2023-08-12 11:14:50 +1200

    make the quote's variable dynamic

--- quoteGenerator/script.js
+++ quoteGenerator/script.js
@@ -1,12 +1,23 @@
-let apiQuotes = []
-// Get Quotes From API
+const quoteContainer = document.getElementById('quote-container')
+const quoteText = document.getElementById('quote')
+const authorText = document.getElementById('author')
+const twitterBtn = document.getElementById('twitter')
+const newQuoteBtn = document.getElementById('new-quote')
 
+let apiQuotes = []
+
+//Show New Quote
+function newQuote() {
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)]
+}
+
+// Get Quotes From API
 async function getQuote() {
   try {
     const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json'
     const res = await fetch(apiUrl)
     apiQuotes = await res.json()
-    console.log(apiQuotes)
+    newQuote()
   } catch (error) {}
 }
 
