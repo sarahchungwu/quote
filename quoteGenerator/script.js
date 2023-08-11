8cf58689d1523f78fc8c41a1883e4345ed5ffcd8
Sarah Zhong <sarahchungwu@gmail.com>
2023-08-12 11:00:46 +1200

    the api worked

--- quoteGenerator/script.js
+++ quoteGenerator/script.js
@@ -1,1 +1,14 @@
+let apiQuotes = []
+// Get Quotes From API
 
+async function getQuote() {
+  try {
+    const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json'
+    const res = await fetch(apiUrl)
+    apiQuotes = await res.json()
+    console.log(apiQuotes)
+  } catch (error) {}
+}
+
+//OnLoad
+getQuote()
