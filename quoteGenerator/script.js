0be8eb0be5a4c38a3d675edf4beacd556ad69230
Sarah Zhong <sarahchungwu@gmail.com>
2023-08-12 11:58:20 +1200

    add the loading process

--- quoteGenerator/script.js
+++ quoteGenerator/script.js
@@ -3,17 +3,34 @@ const quoteText = document.getElementById('quote')
 const authorText = document.getElementById('author')
 const twitterBtn = document.getElementById('twitter')
 const newQuoteBtn = document.getElementById('new-quote')
+const loader = document.getElementById('loader')
 
 let apiQuotes = []
 
+// show Loading
+function loading() {
+  loader.hidden = false
+  quoteContainer.hidden = true
+}
+
+// Hide Loading
+function complete() {
+  quoteContainer.hidden = false
+  loader.hidden = true
+}
+
 //Show New Quote
 function newQuote() {
+  loading()
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)]
   const { author, text } = quote
 
   authorText.textContent = author || 'Unknown'
   quoteText.classList.toggle('long-quote', text.length > 100)
+
+  // Set Quote, Hide Loader
   quoteText.textContent = text
+  complete()
 }
 
 // Get Quotes From API
